2a8502de8424f3a9bbfeb766598658da9179781f
unknown <huangkaizh@163.com>
2019-11-21 14:59:15 +0800

    1、打包bug修复

--- webpack.common.js
+++ webpack.common.js
@@ -7,15 +7,15 @@ module.exports = {
   entry: {
     app: './src/main.js' // 入口文件的位置
   },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      '@': pathJoin(__dirname, '../src')
-    }
-  },
   output: {
     filename: '[name].[hash].js', // 输出文件名字的格式
-    path: pathJoin(__dirname, '../dist') // 输出的绝对路径
+    path: pathJoin(__dirname, './dist') // 输出的绝对路径
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all' // async表示只从异步加载得模块（动态加载import()）里面进行拆分；
+      // initial表示只从入口模块进行拆分；all表示以上两者都包括
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
